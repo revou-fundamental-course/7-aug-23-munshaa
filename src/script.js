62c295d46bc7f5f26f774457d47ba0956539299f
Munsha <88418128+munshaa@users.noreply.github.com>
2023-08-17 23:27:24 +0700

    Update script.js

--- src/script.js
+++ src/script.js
@@ -17,24 +17,3 @@ function hitungkeliling(){
     keliling=parseInt(sisi1)+parseInt(sisi2)+parseInt(sisi3);
     document.getElementById("keliling").value=keliling;
 }
-class Segitiga {
-    constructor (a, t, b) {
-        this.a = a
-        this.t = t
-        this.b = b
-    };
-    kelilingSegitiga () {
-        result = this.a + this.b + this.t;
-        result = result.toPrecision(3)
-        outputDescText = `K = a + b + t <br> K = ${this.a} + ${this.b} + ${this.t} <br> K = ${result} ${lengthUnit}`;
-        outputDesc.innerHTML = outputDescText;
-        kelilingOutput();
-    };
-    luasSegitiga () {
-        result = (this.a * this.t) / 2;
-        result = result.toPrecision(3)
-        outputDescText = `L = a x t ÷ 2 <br> L = ${this.a} x ${this.t} ÷ 2 <br> L = ${result} ${lengthUnit}<sup>2</sup>`;
-        outputDesc.innerHTML = outputDescText;
-        luasOutput();
-    };
-};
